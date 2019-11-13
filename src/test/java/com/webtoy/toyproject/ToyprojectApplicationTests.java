867ace886a3ca1343fba3ffe1757533b4486ef08
dhslrl321 <dhslrl321@gmail.com>
2019-11-13 20:22:16 +0900

    Initial commit

---
+++ src/test/java/com/webtoy/toyproject/ToyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtoy.toyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
